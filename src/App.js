3cc0a7e75a4d4dcfb1bbdebd882ff075f2b542b4
RyouSuzu <22b1307Andrew.ac.jp>
2023-11-14 18:08:08 +0900

    たーくん二号

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src="shasin.jpg"></img>
+        <img src="shasin.jpg" alt='ryouta'></img>
         <h1>公式プロフィール</h1>
         <p>カテゴリー　現役ダンサー</p>
         <p>氏名　木山僚太</p>
         <p>生年月日　2003年7月31日</p>
         <p>主ジャンル　lock</p>
-       <p>他ジャンル　break、アニメーション、KRUMP、HIP-HOP</p>
+        <p>他ジャンル　break、アニメーション、KRUMP、HIP-HOP</p>
         <p>身長　172cm</p>
         <p>体重　61㎏</p>
         <p>趣味　テニス、ダンス、釣り、ゲーム、オタ活</p>
         <p>所属　桃山学院大学、経営学部、経営学科、桃山テック会</p>
+        <p></p>
 
-      
+
       </header>
     </div>
   );
