5a6c89f7fb00af2de39fa382adc7e2bfa439a4d3
RyouSuzu <22b1307Andrew.ac.jp>
2023-11-14 18:03:47 +0900

    たーくん

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This Site is Profile Site By Momoyama Tech Club
-        </p>
-        <p>ryouta is suzuka</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <img src="shasin.jpg"></img>
+        <h1>公式プロフィール</h1>
+        <p>カテゴリー　現役ダンサー</p>
+        <p>氏名　木山僚太</p>
+        <p>生年月日　2003年7月31日</p>
+        <p>主ジャンル　lock</p>
+       <p>他ジャンル　break、アニメーション、KRUMP、HIP-HOP</p>
+        <p>身長　172cm</p>
+        <p>体重　61㎏</p>
+        <p>趣味　テニス、ダンス、釣り、ゲーム、オタ活</p>
+        <p>所属　桃山学院大学、経営学部、経営学科、桃山テック会</p>
+
+      
       </header>
     </div>
   );
