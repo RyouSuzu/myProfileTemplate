065c4fdd7a05636d85bc1458b0b189fe4692687f
RyouSuzu <22b1307Andrew.ac.jp>
2023-11-14 19:57:17 +0900

    たーくん3号s

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
         <img src="shasin.jpg" alt='ryouta'></img>
         <h1>公式プロフィール</h1>
         <p>カテゴリー　現役ダンサー</p>
-        <p>氏名　木山僚太</p>
+        <p>氏名　💩木山僚太💩</p>
         <p>生年月日　2003年7月31日</p>
         <p>主ジャンル　lock</p>
         <p>他ジャンル　break、アニメーション、KRUMP、HIP-HOP</p>
@@ -15,7 +15,7 @@ function App() {
         <p>体重　61㎏</p>
         <p>趣味　テニス、ダンス、釣り、ゲーム、オタ活</p>
         <p>所属　桃山学院大学、経営学部、経営学科、桃山テック会</p>
-        <p>公式X https:twitter.com/Jinyon_0731_CHUS </p>
+        <p>公式X https:twitter.com/Jinyon_0731_CHU</p>
         <a href='https:twitter.com/Jinyon_0731_CHU'>バックダンサー、ショーケース出演、振付作成などのお仕事の依頼はこちらのDMまで</a>
 
 
